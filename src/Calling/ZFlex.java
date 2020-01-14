78a3dd0c48ecc5a56aca8257906beafd4f5742a0
vvikhasta <vvikhasta@gmail.com>
2020-01-14 16:32:53 +0200

    java methods and functions

---
+++ src/Calling/ZFlex.java
@@ -0,0 +1,12 @@
+package Calling;
+
+import Testing.A;
+
+public class ZFlex {
+public static void main(String[] args) {
+	
+	A obj = new A();
+
+	obj.sumFlex(14, 17); // flex mth because variables are undefine in class
+}
+}
